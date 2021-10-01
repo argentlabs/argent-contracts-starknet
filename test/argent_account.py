3317a54486f9200ae53c73782fc00c7108edb817
Julien Niset <julien.niset@gmail.com>
2021-10-01 16:02:54 +0200

    remove cmmented tests

--- test/argent_account.py
+++ test/argent_account.py
@@ -12,7 +12,6 @@ L1_ADDRESS = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984
 
 ESCAPE_SECURITY_PERIOD = 500
 
-
 @pytest.fixture(scope='module')
 def event_loop():
     return asyncio.new_event_loop()
@@ -31,52 +30,52 @@ async def account_factory(get_starknet):
     return starknet, account
 
 
-# @pytest.mark.asyncio
-# async def test_initializer(account_factory):
-#     _, account = account_factory
-#     assert (await account.get_signer().call()).signer == (signer.public_key)
-#     assert (await account.get_guardian().call()).guardian == (guardian.public_key)
-#     assert (await account.get_L1_address().call()).L1_address == (L1_ADDRESS)
+@pytest.mark.asyncio
+async def test_initializer(account_factory):
+    _, account = account_factory
+    assert (await account.get_signer().call()).signer == (signer.public_key)
+    assert (await account.get_guardian().call()).guardian == (guardian.public_key)
+    assert (await account.get_L1_address().call()).L1_address == (L1_ADDRESS)
 
 
-# @pytest.mark.asyncio
-# async def test_execute(account_factory):
-#     starknet, account = account_factory
-#     initializable = await deploy(starknet, "contracts/Initializable.cairo")
+@pytest.mark.asyncio
+async def test_execute(account_factory):
+    starknet, account = account_factory
+    initializable = await deploy(starknet, "contracts/Initializable.cairo")
 
-#     builder = TransactionBuilder(account, signer, guardian)
-#     nonce = await builder.get_current_nonce()
-#     transaction = builder.build_execute_transaction(initializable.contract_address, 'initialize', [], nonce)
+    builder = TransactionBuilder(account, signer, guardian)
+    nonce = await builder.get_current_nonce()
+    transaction = builder.build_execute_transaction(initializable.contract_address, 'initialize', [], nonce)
 
-#     assert await initializable.initialized().call() == (0,)
-#     await transaction.invoke()
-#     assert await initializable.initialized().call() == (1,)
+    assert await initializable.initialized().call() == (0,)
+    await transaction.invoke()
+    assert await initializable.initialized().call() == (1,)
 
-# @pytest.mark.asyncio
-# async def test_change_signer(account_factory):
-#     starknet, account = account_factory
-#     builder = TransactionBuilder(account, signer, guardian)
+@pytest.mark.asyncio
+async def test_change_signer(account_factory):
+    starknet, account = account_factory
+    builder = TransactionBuilder(account, signer, guardian)
 
-#     new_signer = Signer(4444444444)
-#     nonce = await builder.get_current_nonce()
-#     transaction = builder.build_change_signer_transaction(new_signer.public_key, nonce)
+    new_signer = Signer(4444444444)
+    nonce = await builder.get_current_nonce()
+    transaction = builder.build_change_signer_transaction(new_signer.public_key, nonce)
 
-#     assert (await account.get_signer().call()).signer == (signer.public_key)
-#     await transaction.invoke()
-#     assert (await account.get_signer().call()).signer == (new_signer.public_key)
+    assert (await account.get_signer().call()).signer == (signer.public_key)
+    await transaction.invoke()
+    assert (await account.get_signer().call()).signer == (new_signer.public_key)
 
-# @pytest.mark.asyncio
-# async def test_change_guardian(account_factory):
-#     starknet, account = account_factory
-#     builder = TransactionBuilder(account, signer, guardian)
+@pytest.mark.asyncio
+async def test_change_guardian(account_factory):
+    starknet, account = account_factory
+    builder = TransactionBuilder(account, signer, guardian)
 
-#     new_guardian = Signer(55555555)
-#     nonce = await builder.get_current_nonce()
-#     transaction = builder.build_change_guardian_transaction(new_guardian.public_key, nonce)
+    new_guardian = Signer(55555555)
+    nonce = await builder.get_current_nonce()
+    transaction = builder.build_change_guardian_transaction(new_guardian.public_key, nonce)
 
-#     assert (await account.get_guardian().call()).guardian == (guardian.public_key)
-#     await transaction.invoke()
-#     assert (await account.get_guardian().call()).guardian == (new_guardian.public_key)
+    assert (await account.get_guardian().call()).guardian == (guardian.public_key)
+    await transaction.invoke()
+    assert (await account.get_guardian().call()).guardian == (new_guardian.public_key)
 
 @pytest.mark.asyncio
 async def test_trigger_escape_signer(account_factory):

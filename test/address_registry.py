3f81daddea1d9229b3807c698f6ca712821de1cb
Thomas Brillard <thomas.brillard@ledger.fr>
2022-04-13 16:40:45 +0200

    Fixed address_registry.py tests

--- test/address_registry.py
+++ test/address_registry.py
@@ -2,13 +2,13 @@ import pytest
 import asyncio
 from starkware.starknet.testing.starknet import Starknet
 from utils.Signer import Signer
-from utils.utilities import deploy
+from utils.utilities import deploy, str_to_felt
 from utils.TransactionSender import TransactionSender
 
 signer = Signer(123456789987654321)
 guardian = Signer(456789987654321123)
 
-VERSION = 206933405232 # '0.1.0' = 30 2E 31 2E 30 = 0x302E312E30 = 206933405232
+VERSION = str_to_felt('0.2.1')
 L1_ADDRESS = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984
 
 @pytest.fixture(scope='module')
@@ -23,7 +23,8 @@ async def get_starknet():
 @pytest.fixture
 async def account_factory(get_starknet):
     starknet = get_starknet
-    account = await deploy(starknet, "contracts/ArgentAccount.cairo", [signer.public_key, guardian.public_key])
+    account = await deploy(starknet, "contracts/ArgentAccount.cairo")
+    await account.initialize(signer.public_key, guardian.public_key).invoke()
     return account
 
 @pytest.fixture
@@ -47,6 +48,6 @@ async def test_setup_registry(account_factory, registry_factory):
 
     assert (await registry.get_L1_address(account.contract_address).call()).result.res == 0
 
-    await sender.send_transaction(registry.contract_address, 'set_L1_address', [L1_ADDRESS], [signer, guardian])
+    await sender.send_transaction([(registry.contract_address, 'set_L1_address', [L1_ADDRESS])], [signer, guardian])
 
     assert (await registry.get_L1_address(account.contract_address).call()).result.res == L1_ADDRESS

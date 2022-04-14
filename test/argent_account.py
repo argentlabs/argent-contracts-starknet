9313747ecb3a8794258ee8252ae46342a20285f4
Julien Niset <julien@argent.xyz>
2022-04-14 16:14:51 +0200

    accept review changes
    Co-authored-by: delaaxe <1091900+delaaxe@users.noreply.github.com>

--- test/argent_account.py
+++ test/argent_account.py
@@ -36,7 +36,7 @@ async def get_starknet():
     return starknet
 
 def update_starknet_block(starknet, block_number=1, block_timestamp=DEFAULT_TIMESTAMP):
-    starknet.state.state.block_info = BlockInfo(block_number=block_number, block_timestamp=block_timestamp, gas_price = 0)
+    starknet.state.state.block_info = BlockInfo(block_number=block_number, block_timestamp=block_timestamp, gas_price=0)
 
 def reset_starknet_block(starknet):
     update_starknet_block(starknet=starknet)

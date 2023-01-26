b57410edcf84c08b5eb0d22860d08be7a351c903
Leonard Paturel <leonardpaturel@gmail.com>
2023-01-26 11:32:34 +0000

    added target to gitignore, deleted target dir

--- target/debug/deps/contracts-b2a0683a92774127.d
+++
@@ -1,5 +0,0 @@
-/home/leonardpat/work/argent-contracts-starknet/target/debug/deps/contracts-b2a0683a92774127.rmeta: contracts/lib.cairo
-
-/home/leonardpat/work/argent-contracts-starknet/target/debug/deps/contracts-b2a0683a92774127.d: contracts/lib.cairo
-
-contracts/lib.cairo:
--- target/debug/deps/contracts-d9a4720e7a910bfe.d
+++
@@ -1,5 +0,0 @@
-/home/leonardpat/work/argent-contracts-starknet/target/debug/deps/contracts-d9a4720e7a910bfe.rmeta: contracts/lib.cairo
-
-/home/leonardpat/work/argent-contracts-starknet/target/debug/deps/contracts-d9a4720e7a910bfe.d: contracts/lib.cairo
-
-contracts/lib.cairo:

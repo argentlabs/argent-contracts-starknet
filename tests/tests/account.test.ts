d0d75ab92de024850bd3293aa9703acc6ee7231a
gaetbout <gaetan.ansotte@hotmail.com>
2023-05-12 14:14:06 +0200

    revert 3200000 to 320000

--- tests/tests/account.test.ts
+++ tests/tests/account.test.ts
@@ -18,7 +18,7 @@ import {
 
 describe("Test contract: ArgentAccount", function () {
   // Avoid timeout
-  this.timeout(3200000);
+  this.timeout(320000);
 
   let argentAccountClassHash: string;
   // let testDapp: Contract;

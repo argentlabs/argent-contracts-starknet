01815a0e766fa65c0641b8f1b872f5b0aeabac9a
gaetbout <gaetan.ansotte@hotmail.com>
2023-05-12 12:03:12 +0200

    change retry interval

--- tests/tests/shared/constants.ts
+++ tests/tests/shared/constants.ts
@@ -4,7 +4,7 @@ import { loadContract } from "./lib";
 // Polls quickly for a local network
 class FastProvider extends SequencerProvider {
   waitForTransaction(txHash: string, options = {}) {
-    return super.waitForTransaction(txHash, { retryInterval: 250, ...options });
+    return super.waitForTransaction(txHash, { retryInterval: 2000, ...options });
   }
 }
 

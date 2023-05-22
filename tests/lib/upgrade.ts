f7d4ecad439eb500cf762b1764f0da4b68a62511
Sergio Garcia <14262484+sgc-code@users.noreply.github.com>
2023-05-22 16:49:54 +0100

    Update tests/lib/upgrade.ts
    Co-authored-by: delaaxe <1091900+delaaxe@users.noreply.github.com>

--- tests/lib/upgrade.ts
+++ tests/lib/upgrade.ts
@@ -2,7 +2,7 @@ import { Call, CallData } from "starknet";
 import { getOutsideCall } from "./outsideExecution";
 
 export function getUpgradeData(calls: Call[]) {
-  const externalCalls = calls.map((call) => getOutsideCall(call));
+  const externalCalls = calls.map(getOutsideCall);
   return CallData.compile({ externalCalls });
 }
 

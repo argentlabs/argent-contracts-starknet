5761cddaaffe7f9704d165205901492cb3f78e99
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-05-23 10:58:31 +0200

    Add sorting

--- tests/lib/multisig.ts
+++ tests/lib/multisig.ts
@@ -18,15 +18,15 @@ export async function deployMultisig(
   threshold: number,
   signersLength: number,
 ): Promise<MultisigWallet> {
-  const privateKeys = randomPrivateKeys(signersLength);
-  const signers = privateKeys.map(ec.starkCurve.getStarkKey).map(BigInt);
+  const { signers, privateKeys } = sortedRandomSigners(signersLength);
+
   const constructorCalldata = CallData.compile({ threshold, signers });
   const addressSalt = randomPrivateKey();
 
   const contractAddress = hash.calculateContractAddressFromHash(addressSalt, classHash, constructorCalldata, 0);
   await fundAccount(contractAddress);
 
-  const deploymentSigner = new MultisigSigner(privateKeys.slice(0, 1));
+  const deploymentSigner = new MultisigSigner([privateKeys[0]]);
   const account = new Account(provider, contractAddress, deploymentSigner, "1");
   account.signer = new MultisigSigner(privateKeys.slice(0, threshold));
 
@@ -35,4 +35,17 @@ export async function deployMultisig(
 
   const accountContract = await loadContract(account.address);
   return { account, accountContract, privateKeys, signers, receipt };
+}
+
+function sortedRandomSigners(length: number) {
+  const unsortedPrivateKeys = randomPrivateKeys(length);
+  const unsortedSigners = unsortedPrivateKeys.map(ec.starkCurve.getStarkKey).map(BigInt);
+
+  const index = [...Array(length).keys()];
+  const order = index.sort((a, b) => (unsortedSigners[a] < unsortedSigners[b] ? -1 : 1));
+
+  const privateKeys = order.map((i) => unsortedPrivateKeys[i]);
+  const signers = order.map((i) => unsortedSigners[i]);
+
+  return { privateKeys, signers };
 }

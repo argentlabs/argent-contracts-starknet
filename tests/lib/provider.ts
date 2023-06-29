9c2057bd37b90f8839e17219f87a264d26121356
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-06-29 16:23:39 +0300

    Basic deploy script

--- tests/lib/provider.ts
+++ tests/lib/provider.ts
@@ -1,7 +1,7 @@
 import { SequencerProvider } from "starknet";
 
 // Polls quickly for a local network
-class FastProvider extends SequencerProvider {
+export class FastProvider extends SequencerProvider {
   waitForTransaction(txHash: string, options = {}) {
     return super.waitForTransaction(txHash, { retryInterval: 250, ...options });
   }

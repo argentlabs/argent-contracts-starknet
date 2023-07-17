5e183a9b8e6f0c2625f6c9b9171d804912e3b477
Leonard Paturel <leonardpaturel@gmail.com>
2023-07-17 12:41:14 +0100

    updated to release target

--- tests/accountDeclare.test.ts
+++ tests/accountDeclare.test.ts
@@ -26,21 +26,21 @@ describe("ArgentAccount: declare", function () {
     await load();
   });
 
-  it("Expect 'argent/invalid-contract-version' when trying to declare Cairo contract version1 (CASM) ", async function () {
+  it.only("Expect 'argent/invalid-contract-version' when trying to declare Cairo contract version1 (CASM) ", async function () {
     const { account } = await deployAccount(argentAccountClassHash);
     const contract: CompiledSierra = json.parse(
-      readFileSync("./target/dev/argent_contracts_Proxy.sierra.json").toString("ascii"),
+      readFileSync("./target/release/argent_contracts_Proxy.sierra.json").toString("ascii"),
     );
     expectRevertWithErrorMessage("argent/invalid-contract-version", () => account.declare({ contract }));
   });
 
-  it("Expect the account to be able to declare a Cairo contract version2 (SIERRA)", async function () {
+  it.only("Expect the account to be able to declare a Cairo contract version2 (SIERRA)", async function () {
     const { account } = await deployAccount(argentAccountClassHash);
     const contract: CompiledSierra = json.parse(
-      readFileSync("./target/dev/argent_contracts_TestDapp.sierra.json").toString("ascii"),
+      readFileSync("./target/release/argent_contracts_TestDapp.sierra.json").toString("ascii"),
     );
     const casm: CompiledSierraCasm = json.parse(
-      readFileSync("./target/dev/argent_contracts_TestDapp.casm.json").toString("ascii"),
+      readFileSync("./target/release/argent_contracts_TestDapp.casm.json").toString("ascii"),
     );
     const { class_hash, transaction_hash } = await account.declare({ contract, casm });
     await provider.waitForTransaction(transaction_hash);

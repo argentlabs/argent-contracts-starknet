d52fbae952b8852c0fb192ac8bdeefa61011c8fc
Leonard Paturel <leonardpaturel@gmail.com>
2023-07-17 11:51:47 +0100

    rename sierra and casm files

--- tests/accountDeclare.test.ts
+++ tests/accountDeclare.test.ts
@@ -29,14 +29,14 @@ describe("ArgentAccount: declare", function () {
 
   it("Expect 'argent/invalid-contract-version' when trying to declare Cairo contract version1 (CASM) ", async function () {
     const { account } = await deployAccount(argentAccountClassHash);
-    const contract: CompiledSierra = json.parse(readFileSync("./tests/fixtures/Proxy.json").toString("ascii"));
+    const contract: CompiledSierra = json.parse(readFileSync("./target/dev/Proxy.sierra.json").toString("ascii"));
     expectRevertWithErrorMessage("argent/invalid-contract-version", () => account.declare({ contract }));
   });
 
   it("Expect the account to be able to declare a Cairo contract version2 (SIERRA)", async function () {
     const { account } = await deployAccount(argentAccountClassHash);
-    const contract: CompiledSierra = json.parse(readFileSync("./tests/fixtures/TestDapp.json").toString("ascii"));
-    const casm: CompiledSierraCasm = json.parse(readFileSync("./tests/fixtures/TestDapp.casm").toString("ascii"));
+    const contract: CompiledSierra = json.parse(readFileSync("./target/dev/argent_contracts_TestDapp.sierra.json").toString("ascii"));
+    const casm: CompiledSierraCasm = json.parse(readFileSync("./target/dev/argent_contracts_TestDapp.casm.json").toString("ascii"));
     const { class_hash, transaction_hash } = await account.declare({ contract, casm });
     await deployer.waitForTransaction(transaction_hash);
     expect(provider.getCompiledClassByClassHash(class_hash)).to.exist;

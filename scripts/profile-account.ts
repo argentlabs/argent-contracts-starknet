0adb2717dd3dd223ed895abdd04bf09770134852
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-11-24 10:55:43 +0300

    Transaction hash profiling

--- scripts/profile-account.ts
+++ scripts/profile-account.ts
@@ -1,4 +1,3 @@
-import { InvokeFunctionResponse } from "starknet";
 import {
   declareContract,
   declareFixtureContract,
@@ -8,7 +7,7 @@ import {
   deployOldAccount,
   loadContract,
 } from "../tests-integration/lib";
-import { profileGasUsage } from "../tests-integration/lib/gas";
+import { makeProfiler } from "../tests-integration/lib/gas";
 
 const argentAccountClassHash = await declareContract("ArgentAccount");
 const oldArgentAccountClassHash = await declareFixtureContract("OldArgentAccount");
@@ -17,31 +16,14 @@ const testDappClassHash = await declareContract("TestDapp");
 const { contract_address } = await deployer.deployContract({ classHash: testDappClassHash });
 const testDappContract = await loadContract(contract_address);
 
-const ethUsd = 1800n;
-
-const table: Record<string, any> = {};
-
-async function reportProfile(name: string, response: InvokeFunctionResponse) {
-  const report = await profileGasUsage(response);
-  const { actualFee, gasUsed, computationGas, l1CalldataGas, executionResources } = report;
-  console.dir(report, { depth: null });
-  const feeUsd = Number(actualFee * ethUsd) / Number(10n ** 18n);
-  table[name] = {
-    actualFee: Number(actualFee),
-    feeUsd: Number(feeUsd.toFixed(2)),
-    gasUsed: Number(gasUsed),
-    computationGas: Number(computationGas),
-    l1CalldataGas: Number(l1CalldataGas),
-    ...executionResources,
-  };
-}
+const profiler = makeProfiler();
 
 {
   const name = "Old Account";
   console.log(name);
   const { account } = await deployOldAccount(proxyClassHash, oldArgentAccountClassHash);
   testDappContract.connect(account);
-  await reportProfile(name, await testDappContract.set_number(42));
+  await profiler.profile(name, await testDappContract.set_number(42));
 }
 
 {
@@ -49,7 +31,7 @@ async function reportProfile(name: string, response: InvokeFunctionResponse) {
   console.log(name);
   const { account } = await deployAccount(argentAccountClassHash);
   testDappContract.connect(account);
-  await reportProfile(name, await testDappContract.set_number(42));
+  await profiler.profile(name, await testDappContract.set_number(42));
 }
 
 {
@@ -57,7 +39,7 @@ async function reportProfile(name: string, response: InvokeFunctionResponse) {
   console.log(name);
   const { account } = await deployAccountWithoutGuardian(argentAccountClassHash);
   testDappContract.connect(account);
-  await reportProfile(name, await testDappContract.set_number(42));
+  await profiler.profile(name, await testDappContract.set_number(42));
 }
 
-console.table(table);
+profiler.printReport();

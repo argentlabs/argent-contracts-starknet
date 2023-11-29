b69c5873f3cac059bfe569a6e0cdea43252959a9
gaetbout <gaetan.ansotte@hotmail.com>
2023-11-29 14:51:56 +0100

    eth_signer to camelCase

--- scripts/generate-signatures.ts
+++ scripts/generate-signatures.ts
@@ -50,9 +50,9 @@ function calculate_account_signature() {
 async function calculate_account_signature_with_eth() {
   // Ethers requires hash to be pair length
   const hash = "0x02d6479c0758efbb5aa07d35ed5454d728637fceab7ba544d3ea95403a5630a8";
-  const eth_signer = new Wallet(id("9n"));
+  const ethSigner = new Wallet(id("9n"));
   const [owner_r, owner_s] = owner.signHash(hash);
-  const signature = Signature.from(eth_signer.signingKey.sign(hash));
+  const signature = Signature.from(ethSigner.signingKey.sign(hash));
 
   console.log(`
     const message_hash: felt252 = ${num.toHex(hash)};
@@ -61,7 +61,7 @@ async function calculate_account_signature_with_eth() {
     const owner_r: felt252 = ${num.toHex(owner_r)};
     const owner_s: felt252 = ${num.toHex(owner_s)};
 
-    const owner_pubkey_eth: felt252 = ${eth_signer.address};
+    const owner_pubkey_eth: felt252 = ${ethSigner.address};
     const owner_eth_r: u256 = ${signature.r};
     const owner_eth_s: u256 = ${signature.s};
     const owner_eth_v: felt252 = ${signature.v};

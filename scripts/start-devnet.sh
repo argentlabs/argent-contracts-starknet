570901e59c0f5e979ea56cf06112f082dceb01c8
Sergio Garcia <sergio@argent.xyz>
2023-12-11 15:51:30 +0000

    update devnet and starknet.js

--- scripts/start-devnet.sh
+++ scripts/start-devnet.sh
@@ -4,5 +4,5 @@ if nc -z 127.0.0.1 5050; then
   exit 1
 else
   echo "Starting Devnet"
-  docker run -p 127.0.0.1:5050:5050 shardlabs/starknet-devnet-rs:64c425b832b96ba09b49646fe0fbf49862c0fb6d --gas-price 36000000000 --timeout 320 --seed 0
+  docker run -p 127.0.0.1:5050:5050 shardlabs/starknet-devnet-rs:78527decb3f76c4c808fa35f46228557af3df385 --gas-price 36000000000 --timeout 320 --seed 0
 fi

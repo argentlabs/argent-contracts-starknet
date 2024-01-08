221fe043b6ea16e84ae16f14c790b440015bd584
Sergio Garcia <sergio@argent.xyz>
2024-01-08 15:44:54 +0000

    simplify

--- scripts/send-v3-invoke.ts
+++ scripts/send-v3-invoke.ts
@@ -7,13 +7,7 @@ const strk = await getStrkContract();
 const call = await strk.populateTransaction.transfer(deployerV3.address, uint256.bnToUint256(10000000000000000000000n));
 ///////////////////////////////////////////////////////////////
 
-const calls: Array<Call> = [
-  {
-    contractAddress: call.contractAddress,
-    calldata: call.calldata,
-    entrypoint: call.entrypoint,
-  },
-];
+const calls: Array<Call> = [call];
 
 const executionResult = await deployerV3.execute(calls);
 console.log(`transaction_hash: ${executionResult.transaction_hash}`);

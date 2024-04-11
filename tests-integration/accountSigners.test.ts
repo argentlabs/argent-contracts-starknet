9b2ae5b7eb0b81fc72fa8273cfa6191896a56524
gaetbout <gaetan.ansotte@hotmail.com>
2024-04-11 11:51:56 +0200

    first draft

--- tests-integration/accountSigners.test.ts
+++ tests-integration/accountSigners.test.ts
@@ -1,23 +1,72 @@
-import { Contract, uint256 } from "starknet";
-import { deployAccount, getEthContract } from "../lib";
+import { uint256 } from "starknet";
+import {
+  ArgentAccount,
+  ArgentSigner,
+  EthKeyPair,
+  Secp256r1KeyPair,
+  StarknetKeyPair,
+  WebauthnOwner,
+  declareFixtureContract,
+  deployAccount,
+  deployAccountWithoutGuardian,
+  ensureSuccess,
+  expectRevertWithErrorMessage,
+  getEthContract,
+  waitForTransaction,
+} from "../lib";
 
-describe.only("ArgentAccount: testing all signers", function () {
-  const accounts: any[] = [];
-  let ethContract: Contract;
-  const recipient = "0xadbe1";
-  const amount = uint256.bnToUint256(1);
+// For some reason I have to put this out of the describe...
+// Otherwise the accounts array is empty
+interface Account {
+  name: string;
+  account: ArgentAccount;
+}
 
-  before(async () => {
-    ethContract = await getEthContract();
-    const { account } = await deployAccount();
-    accounts.push({ name: "regular account", account });
-    console.log(accounts);
-  });
+const accounts: Account[] = [];
+const recipient = "0xadbe1";
+const amount = uint256.bnToUint256(1);
+const ethContract = await getEthContract();
+await declareFixtureContract("Sha256Cairo0");
 
+const starknetKeyPairs = [{ name: "Starknet signature", keyPair: new StarknetKeyPair() }];
+
+const nonStarknetKeyPairs = [
+  { name: "Ethereum signature", keyPair: new EthKeyPair() },
+  { name: "Secp256r1 signature", keyPair: new Secp256r1KeyPair() },
+  { name: "Eip191 signature", keyPair: new Secp256r1KeyPair() },
+  { name: "Webauthn signature", keyPair: new WebauthnOwner() },
+];
+
+for (const { name, keyPair } of [...starknetKeyPairs, ...nonStarknetKeyPairs]) {
+  const { account: withGuardian } = await deployAccount({ owner: keyPair });
+  accounts.push({ name, account: withGuardian });
+  const { account: withoutGuardian } = await deployAccountWithoutGuardian({ owner: keyPair });
+  accounts.push({ name: name + " (without guardian)", account: withoutGuardian });
+}
+
+describe("ArgentAccount: testing all signers", function () {
   for (const { name, account } of accounts) {
-    it.only(`Testing ${name}`, async function () {
-      // ethContract.connect(account);
-      // await ethContract.transfer(recipient, amount);
+    it(`Testing "${name}"`, async function () {
+      ethContract.connect(account);
+      await ensureSuccess(await waitForTransaction(await ethContract.transfer(recipient, amount)));
+    });
+  }
+
+  for (const { name, keyPair } of nonStarknetKeyPairs) {
+    it(`Deploying with a wrong guardian "${name}"`, async function () {
+      await expectRevertWithErrorMessage("argent/invalid-guardian-type", async () => {
+        const { transactionHash } = await deployAccount({ guardian: keyPair });
+        return { transaction_hash: transactionHash };
+      });
+    });
+
+    it(`trigger_escape_guardian "${name}"`, async function () {
+      const { accountContract, account, owner } = await deployAccount();
+      account.signer = new ArgentSigner(owner);
+      await expectRevertWithErrorMessage(
+        "argent/invalid-guardian-type", () => 
+        accountContract.trigger_escape_guardian(keyPair.compiledSignerAsOption),
+      );
     });
   }
 });

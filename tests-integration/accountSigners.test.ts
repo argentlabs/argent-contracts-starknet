9605457f96a4e1c208b96325741917734706625b
gaetbout <gaetan.ansotte@hotmail.com>
2024-04-11 16:27:48 +0200

    fixing tests

--- tests-integration/accountSigners.test.ts
+++ tests-integration/accountSigners.test.ts
@@ -1,70 +1,78 @@
-import { uint256 } from "starknet";
+import { Contract, uint256 } from "starknet";
 import {
   ArgentAccount,
   ArgentSigner,
-  EthKeyPair,
-  Secp256r1KeyPair,
-  StarknetKeyPair,
-  WebauthnOwner,
   declareFixtureContract,
   deployAccount,
   deployAccountWithoutGuardian,
   ensureSuccess,
   expectRevertWithErrorMessage,
   getEthContract,
+  randomEip191KeyPair,
+  randomEthKeyPair,
+  randomSecp256r1KeyPair,
+  randomStarknetKeyPair,
+  randomWebauthnOwner,
   waitForTransaction,
 } from "../lib";
 
-// For some reason I have to put this out of the describe...
-// Otherwise the accounts array is empty
 interface Account {
   name: string;
   account: ArgentAccount;
 }
 
-const accounts: Account[] = [];
-const recipient = "0xadbe1";
-const amount = uint256.bnToUint256(1);
-const ethContract = await getEthContract();
-await declareFixtureContract("Sha256Cairo0");
+describe("ArgentAccount", function () {
+  const recipient = "0xadbe1";
+  const amount = uint256.bnToUint256(1);
+  let ethContract: Contract;
 
-const starknetKeyPairs = [{ name: "Starknet signature", keyPair: new StarknetKeyPair() }];
+  const accounts: Account[] = [];
+  const starknetKeyPairs = [{ name: "Starknet signature", keyPair: randomStarknetKeyPair }];
 
-const nonStarknetKeyPairs = [
-  { name: "Ethereum signature", keyPair: new EthKeyPair() },
-  { name: "Secp256r1 signature", keyPair: new Secp256r1KeyPair() },
-  { name: "Eip191 signature", keyPair: new Secp256r1KeyPair() },
-  { name: "Webauthn signature", keyPair: new WebauthnOwner() },
-];
+  const nonStarknetKeyPairs = [
+    { name: "Ethereum signature", keyPair: randomEthKeyPair },
+    { name: "Secp256r1 signature", keyPair: randomSecp256r1KeyPair },
+    { name: "Eip191 signature", keyPair: randomEip191KeyPair },
+    { name: "Webauthn signature", keyPair: randomWebauthnOwner },
+  ];
 
-for (const { name, keyPair } of [...starknetKeyPairs, ...nonStarknetKeyPairs]) {
-  const { account: withGuardian } = await deployAccount({ owner: keyPair });
-  accounts.push({ name, account: withGuardian });
-  const { account: withoutGuardian } = await deployAccountWithoutGuardian({ owner: keyPair });
-  accounts.push({ name: name + " (without guardian)", account: withoutGuardian });
-}
+  before(async () => {
+    ethContract = await getEthContract();
+    await declareFixtureContract("Sha256Cairo0");
 
-describe("ArgentAccount: testing all signers", function () {
-  for (const { name, account } of accounts) {
-    it(`Testing "${name}"`, async function () {
-      ethContract.connect(account);
-      await ensureSuccess(await waitForTransaction(await ethContract.transfer(recipient, amount)));
+    for (const { name, keyPair } of [...starknetKeyPairs, ...nonStarknetKeyPairs]) {
+      const { account: withGuardian } = await deployAccount({ owner: keyPair() });
+      accounts.push({ name, account: withGuardian });
+      const { account: withoutGuardian } = await deployAccountWithoutGuardian({ owner: keyPair() });
+      accounts.push({ name: name + " (without guardian)", account: withoutGuardian });
+    }
+  });
+
+  it("Waiting accounts to be filled", function () {
+    describe("Simple transfer", 
+     function () {
+      for (const { name, account } of accounts) {
+        it(`Using "${name}"`, async function () {
+          ethContract.connect(account);
+          await ensureSuccess(await waitForTransaction(await ethContract.transfer(recipient, amount)));
+        });
+      }
     });
-  }
+  });
 
   for (const { name, keyPair } of nonStarknetKeyPairs) {
-    it(`Deploying with a wrong guardian "${name}"`, async function () {
+    it(`Expect 'argent/invalid-guardian-type' when deploying with a wrong guardian "${name}"`, async function () {
       await expectRevertWithErrorMessage("argent/invalid-guardian-type", async () => {
-        const { transactionHash } = await deployAccount({ guardian: keyPair });
+        const { transactionHash } = await deployAccount({ guardian: keyPair() });
         return { transaction_hash: transactionHash };
       });
     });
 
-    it(`trigger_escape_guardian "${name}"`, async function () {
+    it(`Expect 'argent/invalid-guardian-type' on trigger_escape_guardian with "${name}"`, async function () {
       const { accountContract, account, owner } = await deployAccount();
       account.signer = new ArgentSigner(owner);
       await expectRevertWithErrorMessage("argent/invalid-guardian-type", () =>
-        accountContract.trigger_escape_guardian(keyPair.compiledSignerAsOption),
+        accountContract.trigger_escape_guardian(keyPair().compiledSignerAsOption),
       );
     });
   }
--- tests-integration/multisigSigners.test.ts
+++ tests-integration/multisigSigners.test.ts
@@ -1,4 +1,4 @@
-import { uint256 } from "starknet";
+import { Contract, uint256 } from "starknet";
 import {
   ArgentAccount,
   declareFixtureContract,
@@ -13,48 +13,52 @@ import {
   sortByGuid,
   waitForTransaction,
 } from "../lib";
-
-// For some reason I have to put this out of the describe...
-// Otherwise the accounts array is empty
 interface Account {
   name: string;
   account: ArgentAccount;
 }
 
-const accounts: Account[] = [];
-const recipient = "0xadbe1";
-const amount = uint256.bnToUint256(1);
-const ethContract = await getEthContract();
-await declareFixtureContract("Sha256Cairo0");
+describe("Multisig: Signers types", function () {
+  const accounts: Account[] = [];
+  const recipient = "0xadbe1";
+  const amount = uint256.bnToUint256(1);
+  let ethContract: Contract;
+  const keyPairs = [
+    { name: "Starknet signature", keyPair: randomStarknetKeyPair },
+    { name: "Ethereum signature", keyPair: randomEthKeyPair },
+    { name: "Secp256r1 signature", keyPair: randomSecp256r1KeyPair },
+    { name: "Eip191 signature", keyPair: randomEip191KeyPair },
+    { name: "Webauthn signature", keyPair: randomWebauthnOwner },
+  ];
 
-const keyPairs = [
-  { name: "Starknet signature", keyPair: randomStarknetKeyPair },
-  { name: "Ethereum signature", keyPair: randomEthKeyPair },
-  { name: "Secp256r1 signature", keyPair: randomSecp256r1KeyPair },
-  { name: "Eip191 signature", keyPair: randomEip191KeyPair },
-  { name: "Webauthn signature", keyPair: randomWebauthnOwner },
-];
+  before(async () => {
+    ethContract = await getEthContract();
+    await declareFixtureContract("Sha256Cairo0");
 
-for (const { name, keyPair } of keyPairs) {
-  const { account: oneSigner } = await deployMultisig({ threshold: 1, keys: [keyPair()] });
-  accounts.push({ name: "1 " + name, account: oneSigner });
+    for (const { name, keyPair } of keyPairs) {
+      const { account: oneSigner } = await deployMultisig({ threshold: 1, keys: [keyPair()] });
+      accounts.push({ name: "1 " + name, account: oneSigner });
 
-  const keys = [...Array(5)].map(() => keyPair());
-  sortByGuid(keys);
-  const { account: fiveSigners } = await deployMultisig({ threshold: 5, keys });
-  accounts.push({ name: "5 " + name, account: fiveSigners });
-}
+      const keys = [...Array(5)].map(() => keyPair());
+      sortByGuid(keys);
+      const { account: fiveSigners } = await deployMultisig({ threshold: 5, keys });
+      accounts.push({ name: "5 " + name, account: fiveSigners });
+    }
 
-const allKeys = keyPairs.map((k) => k.keyPair());
-sortByGuid(allKeys);
-const { account } = await deployMultisig({ threshold: 5, keys: allKeys });
-accounts.push({ name: "One of each", account });
+    const allKeys = keyPairs.map((k) => k.keyPair());
+    sortByGuid(allKeys);
+    const { account } = await deployMultisig({ threshold: 5, keys: allKeys });
+    accounts.push({ name: "One of each", account });
+  });
 
-describe("Multisig: testing all signers", function () {
-  for (const { name, account } of accounts) {
-    it(`Testing "${name}"`, async function () {
-      ethContract.connect(account);
-      await ensureSuccess(await waitForTransaction(await ethContract.transfer(recipient, amount)));
+  it("Waiting accounts to be filled", function () {
+    describe("Simple transfer", function () {
+      for (const { name, account } of accounts) {
+        it(`Using "${name}"`, async function () {
+          ethContract.connect(account);
+          await ensureSuccess(await waitForTransaction(await ethContract.transfer(recipient, amount)));
+        });
+      }
     });
-  }
+  });
 });

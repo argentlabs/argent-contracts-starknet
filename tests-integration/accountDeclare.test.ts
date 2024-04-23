cfae8173aee27feb862bfd1de1d22ae9fb2bb051
gaetbout <gaetan.ansotte@hotmail.com>
2024-04-23 12:20:48 +0200

    first draft

--- tests-integration/accountDeclare.test.ts
+++ tests-integration/accountDeclare.test.ts
@@ -14,13 +14,16 @@ describe("ArgentAccount: declare", function () {
   beforeEach(async () => {
     await restartDevnet();
   });
-  for (const useTxV3 of [false, true]) {
-    it(`Expect 'argent/invalid-contract-version' when trying to declare Cairo contract version1 (CASM) (TxV3: ${useTxV3})`, async function () {
-      const { account } = await deployAccount({ useTxV3 });
-      const contract: CompiledSierra = readContract(`${fixturesFolder}Proxy.contract_class.json`);
-      expectRevertWithErrorMessage("argent/invalid-tx-version", () => account.declare({ contract }));
-    });
 
+  it(`Expect 'argent/invalid-contract-version' when trying to declare Cairo contract version1 (CASM) (TxV3: false)`, async function () {
+    const { account } = await deployAccount({ useTxV3: false });
+    const contract: CompiledSierra = readContract(`${fixturesFolder}Proxy.contract_class.json`);
+    await expectRevertWithErrorMessage("argent/invalid-declare-version", () =>
+      account.declare({ contract }, { version: "0x1" }),
+    );
+  });
+
+  for (const useTxV3 of [false, true]) {
     it(`Expect the account to be able to declare a Cairo contract version2 (SIERRA) (TxV3:${useTxV3})`, async function () {
       const { account } = await deployAccount({ useTxV3 });
       const { class_hash: mockDappClassHash } = await account.declare(getDeclareContractPayload("MockDapp"));

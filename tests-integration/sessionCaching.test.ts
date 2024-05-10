71b72f73c63c507ab8d13e05de50f75cc7d80531
Leonard Paturel <leonardpaturel@gmail.com>
2024-05-10 14:12:41 +0100

    added test in loop

--- tests-integration/sessionCaching.test.ts
+++ tests-integration/sessionCaching.test.ts
@@ -35,47 +35,46 @@ describe("Hybrid Session Account: execute session calls with caching", function 
     await manager.setTime(initialTime);
   });
 
-  it("Use Session with caching enabled", async function () {
-    const { accountContract, account, guardian } = await deployAccount({
-      classHash: sessionAccountClassHash,
-    });
-
-    const allowedMethods: AllowedMethod[] = [
-      {
-        "Contract Address": mockDappContract.address,
-        selector: "set_number_double",
-      },
-    ];
-
-    const calls = [mockDappContract.populateTransaction.set_number_double(2)];
-
-    const { accountWithDappSigner, sessionHash } = await setupSession(
-      guardian as StarknetKeyPair,
-      account,
-      allowedMethods,
-      initialTime + 150n,
-      randomStarknetKeyPair(),
-      true,
-    );
-
-    await accountContract.is_session_authorization_cached(sessionHash).should.eventually.be.false;
-    const { transaction_hash } = await accountWithDappSigner.execute(calls);
-
-    await account.waitForTransaction(transaction_hash);
-    await mockDappContract.get_number(accountContract.address).should.eventually.equal(4n);
-
-    // check that the session is cached
-    await accountContract.is_session_authorization_cached(sessionHash).should.eventually.be.true;
-
-    const calls2 = [mockDappContract.populateTransaction.set_number_double(4)];
-
-    const { transaction_hash: tx2 } = await accountWithDappSigner.execute(calls2);
-
-    await account.waitForTransaction(tx2);
-    await mockDappContract.get_number(accountContract.address).should.eventually.equal(8n);
-  });
-
   for (const useCaching of [false, true]) {
+    it("Session is successfully cached", async function () {
+      const { accountContract, account, guardian } = await deployAccount({
+        classHash: sessionAccountClassHash,
+      });
+
+      const allowedMethods: AllowedMethod[] = [
+        {
+          "Contract Address": mockDappContract.address,
+          selector: "set_number_double",
+        },
+      ];
+
+      const calls = [mockDappContract.populateTransaction.set_number_double(2)];
+
+      const { accountWithDappSigner, sessionHash } = await setupSession(
+        guardian as StarknetKeyPair,
+        account,
+        allowedMethods,
+        initialTime + 150n,
+        randomStarknetKeyPair(),
+        true,
+      );
+
+      await accountContract.is_session_authorization_cached(sessionHash).should.eventually.be.false;
+      const { transaction_hash } = await accountWithDappSigner.execute(calls);
+
+      await account.waitForTransaction(transaction_hash);
+      await mockDappContract.get_number(accountContract.address).should.eventually.equal(4n);
+
+      // check that the session is cached
+      await accountContract.is_session_authorization_cached(sessionHash).should.eventually.be.true;
+
+      const calls2 = [mockDappContract.populateTransaction.set_number_double(4)];
+
+      const { transaction_hash: tx2 } = await accountWithDappSigner.execute(calls2);
+
+      await account.waitForTransaction(tx2);
+      await mockDappContract.get_number(accountContract.address).should.eventually.equal(8n);
+    });
     it(`Fail if guardian backup signed session (caching: ${useCaching})`, async function () {
       const { account, guardian, accountContract } = await deployAccountWithGuardianBackup({
         classHash: sessionAccountClassHash,

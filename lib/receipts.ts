a6af76c7d934120ae234389c40ee51aa5de8a2de
Leonard Paturel <leonardpaturel@gmail.com>
2024-07-18 16:07:01 +0100

    better type accpetance for ensureAccepted

--- lib/receipts.ts
+++ lib/receipts.ts
@@ -5,17 +5,20 @@ import { manager } from "./manager";
 export async function ensureSuccess(
   transactionOrHash: { transaction_hash: string } | string,
 ): Promise<TransactionReceipt> {
-  const transaction_hash =
+  const transactionHash =
     typeof transactionOrHash === "string" ? transactionOrHash : transactionOrHash.transaction_hash;
-  const tx = await manager.waitForTransaction(transaction_hash, {
+  const tx = await manager.waitForTransaction(transactionHash, {
     successStates: [TransactionFinalityStatus.ACCEPTED_ON_L1, TransactionFinalityStatus.ACCEPTED_ON_L2],
   });
-  assert(tx.isSuccess(), `Transaction ${transaction_hash} REVERTED`);
+  assert(tx.isSuccess(), `Transaction ${transactionHash} REVERTED`);
   return tx;
 }
 
-export async function ensureAccepted(receiptOrHash: TransactionReceipt | string): Promise<TransactionReceipt> {
-  const transactionHash = typeof receiptOrHash === "string" ? receiptOrHash : receiptOrHash.transaction_hash;
+export async function ensureAccepted(
+  transactionOrHash: { transaction_hash: string } | string,
+): Promise<TransactionReceipt> {
+  const transactionHash =
+    typeof transactionOrHash === "string" ? transactionOrHash : transactionOrHash.transaction_hash;
   const receipt = await manager.waitForTransaction(transactionHash, {
     successStates: [TransactionFinalityStatus.ACCEPTED_ON_L1, TransactionFinalityStatus.ACCEPTED_ON_L2],
   });

fe91ee674c005dab1a0ee9c6a6463f366dfcfd45
Leonard Paturel <leonardpaturel@gmail.com>
2024-07-22 18:07:51 +0200

    update export index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -23,6 +23,7 @@ export * from "./signers/secp256";
 export * from "./signers/signers";
 export * from "./signers/webauthn";
 export * from "./tokens";
+export * from "./transactions";
 export * from "./udc";
 export * from "./upgrade";
 

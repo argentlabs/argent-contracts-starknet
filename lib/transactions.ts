05e37558e00482adc38c3efc681acd08ae74c928
Leonard-Pat <106159231+Leonard-Pat@users.noreply.github.com>
2024-07-26 13:48:22 +0100

    Update transactions.ts
    Co-authored-by: gaetbout <gaetan.ansotte@hotmail.com>

--- lib/transactions.ts
+++ lib/transactions.ts
@@ -11,17 +11,16 @@ import {
 
 export function calculateTransactionHash(transactionDetail: InvocationsSignerDetails, calls: Call[]): string {
   const compiledCalldata = transaction.getExecuteCalldata(calls, transactionDetail.cairoVersion);
-  let transactionHash: string;
   if (Object.values(RPC.ETransactionVersion2).includes(transactionDetail.version as any)) {
     const transactionDetailV2 = transactionDetail as V2InvocationsSignerDetails;
-    transactionHash = hash.calculateInvokeTransactionHash({
+    return hash.calculateInvokeTransactionHash({
       ...transactionDetailV2,
       senderAddress: transactionDetailV2.walletAddress,
       compiledCalldata,
     });
   } else if (Object.values(RPC.ETransactionVersion3).includes(transactionDetail.version as any)) {
     const transactionDetailV3 = transactionDetail as V3InvocationsSignerDetails;
-    transactionHash = hash.calculateInvokeTransactionHash({
+    return hash.calculateInvokeTransactionHash({
       ...transactionDetailV3,
       senderAddress: transactionDetailV3.walletAddress,
       compiledCalldata,
@@ -31,5 +30,4 @@ export function calculateTransactionHash(transactionDetail: InvocationsSignerDet
   } else {
     throw Error("unsupported transaction version");
   }
-  return transactionHash;
 }

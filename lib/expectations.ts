4d65e85386216bd038892430c72fa7e748bf9a05
gaetbout <gaetan.ansotte@hotmail.com>
2024-07-29 15:42:10 +0200

    fixing all ts-tests

--- lib/expectations.ts
+++ lib/expectations.ts
@@ -28,7 +28,6 @@ export async function expectExecutionRevert(errorMessage: string, execute: Promi
     await manager.waitForTx(execute);
     /* eslint-disable  @typescript-eslint/no-explicit-any */
   } catch (e: any) {
-    console.log(e);
     expect(e.toString()).to.contain(`Failure reason: ${shortString.encodeShortString(errorMessage)}`);
     return;
   }

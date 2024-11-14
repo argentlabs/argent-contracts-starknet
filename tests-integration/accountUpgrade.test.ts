76d7bcd639d84146348ce4a265a654ab0ae85b6d
gaetbout <gaetan.ansotte@hotmail.com>
2024-11-14 11:28:57 +0100

    more renaming

--- tests-integration/accountUpgrade.test.ts
+++ tests-integration/accountUpgrade.test.ts
@@ -22,31 +22,31 @@ describe("ArgentAccount: upgrade", function () {
   before(async () => {
     argentAccountClassHash = await manager.declareLocalContract("ArgentAccount");
     mockDapp = await manager.deployContract("MockDapp");
-    const ca1 = await manager.declareArtifactContract(
+    const classHashV030 = await manager.declareArtifactContract(
       "/account-0.3.0-0x1a736d6ed154502257f02b1ccdf4d9d1089f80811cd6acad48e6b6a9d1f2003/ArgentAccount",
     );
     upgradeData.push({
-      deployAccount: async () => deployLegacyAccount(ca1),
+      deployAccount: async () => deployLegacyAccount(classHashV030),
       newOwner: 12,
       newGuardian: 12,
       toSigner: (x: any) => x,
     });
 
-    const ca2 = await manager.declareArtifactContract(
+    const classHashV031 = await manager.declareArtifactContract(
       "/account-0.3.1-0x29927c8af6bccf3f6fda035981e765a7bdbf18a2dc0d630494f8758aa908e2b/ArgentAccount",
     );
     upgradeData.push({
-      deployAccount: async () => deployLegacyAccount(ca2),
+      deployAccount: async () => deployLegacyAccount(classHashV031),
       newOwner: 12,
       newGuardian: 12,
       toSigner: (x: any) => x,
     });
 
-    const ca3 = await manager.declareArtifactContract(
+    const classHashV040 = await manager.declareArtifactContract(
       "/account-0.4.0-0x036078334509b514626504edc9fb252328d1a240e4e948bef8d0c08dff45927f/ArgentAccount",
     );
     upgradeData.push({
-      deployAccount: async () => deployAccount({ classHash: ca3 }),
+      deployAccount: async () => deployAccount({ classHash: classHashV040 }),
       newOwner: randomStarknetKeyPair().compiledSigner,
       newGuardian: new CairoOption(CairoOptionVariant.None),
       toSigner: (x: any) => new ArgentSigner(x),

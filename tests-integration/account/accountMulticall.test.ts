8fc1d5a819a3fc9a05796119caa32f185fa27078
gaetbout <gaetan.ansotte@hotmail.com>
2025-02-13 11:30:21 +0100

    2 comments

--- tests-integration/account/accountMulticall.test.ts
+++ tests-integration/account/accountMulticall.test.ts
@@ -35,13 +35,12 @@ describe("ArgentAccount: multicall", function () {
     });
   });
 
-  it("Should be possible to send eth with a Cairo1 account using a multicall", async function () {
+  it("Should be possible to send strk with a Cairo1 account using a multicall", async function () {
     const { account } = await deployAccount();
     const recipient1 = "42";
     const amount1 = uint256.bnToUint256(1000);
     const recipient2 = "43";
     const amount2 = uint256.bnToUint256(42000);
-    await manager.tokens.fundAccount(account.address, 1000 + 42000, "ETH");
 
     const senderInitialBalance = await manager.tokens.strkBalance(account.address);
     const recipient1InitialBalance = await manager.tokens.strkBalance(recipient1);

1e31bbf94ca052978554f969f501d6a86626c53c
gaetbout <gaetan.ansotte@hotmail.com>
2025-09-25 15:37:23 +0200

    remove outdated casm declare test

--- tests-integration/account/accountDeclare.test.ts
+++ tests-integration/account/accountDeclare.test.ts
@@ -1,19 +1,15 @@
 import { expect } from "chai";
-import { CompiledSierra } from "starknet";
-import { deployAccount, fixturesFolder, getDeclareContractPayload, manager, readContract } from "../../lib";
+import {
+  deployAccount,
+  getDeclareContractPayload,
+  manager,
+} from "../../lib";
 
 describe("ArgentAccount: declare", function () {
   beforeEach(async () => {
     await manager.restartDevnetAndClearClassCache();
   });
 
-  it(`Expect 'argent/invalid-contract-version' when trying to declare Cairo contract version1 (CASM)`, async function () {
-    const { account } = await deployAccount();
-    // Using version 1 will require ETH
-    await manager.mintEth(account.address, 1e18);
-    const contract: CompiledSierra = readContract(`${fixturesFolder}Proxy.contract_class.json`);
-    // await expectRevertWithErrorMessage("argent/invalid-declare-version", account.declare({ contract }));
-  });
   it(`Expect the account to be able to declare a Cairo contract version2 (SIERRA)`, async function () {
     const { account } = await deployAccount();
     const { class_hash: mockDappClassHash } = await account.declare(getDeclareContractPayload("MockDapp"));
